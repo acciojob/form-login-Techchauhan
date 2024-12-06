72d4b50c855bb99545dcbb113a58f8a6abcc3689
Techchauhan <88832979+Techchauhan@users.noreply.github.com>
2024-12-06 12:41:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
 
+	event.preventDefault();
+
+	var firstName = document.forms[form1][fname].value;
+	var lastName = document.froms[form1][lname].value;
+
+	var message = `${firstName} ${lastName}`
+	alert(message);
+	
+
 }
